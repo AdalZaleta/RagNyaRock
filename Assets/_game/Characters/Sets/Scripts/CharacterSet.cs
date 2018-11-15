3a6c68159f4c332076e38af213dfb38e67ef307c
Ngakk <ngakwastaken@gmail.com>
2018-11-15 08:50:45 -0600

    inicio ui

---
+++ Assets/_game/Characters/Sets/Scripts/CharacterSet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos {
+    public class CharacterSet : ScriptableObject {
+        public string namae;
+        public Sprite icon;
+        public SkinInfo[] skins;
+
+        public void BuildCharacter(int _skin)
+        {
+            //get icon, model and everything and build character for game and sprites for selection screen
+        }
+    }
+}
---
+++ Assets/_game/Characters/Sets/Scripts/SkinInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+    public class SkinInfo : ScriptableObject
+    {
+        public string namae;
+        public Sprite splash;
+        public GameObject model;
+    }
+}
