6ce091507b0d4b3125be09b6e7b775e5b65d79d2
Ngakk <ngakwastaken@gmail.com>
2018-11-15 08:11:32 -0600

    MANGOS

---
+++ Assets/_game/MANGOS/Manager_Static.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+
+    //ES EL MANAGER STATICO ES UN SCRIPT QUE SE COMUNICA CON TODOS LOS DEMAS SCRIPTS SIN IMPORTA SI ESTA EN LA ESCENA
+
+    //ESTE ES UN ENUMERADOR QUE NOS DICE LOS ESTADOS DE LA APLICACION
+    public enum AppState
+    {
+        PAUSE,
+        GAMEPLAY,
+        CREDITS,
+        GAME_END,
+        MAIN_MENU
+    }
+
+    public enum GameState
+    {
+        
+    }
+
+    //ESTE SE ENCARGARA DE MANTENER A LOS DEMAS MANAGER COMUNICADOS ENTRE ELLOS
+    public static class Manager_Static
+	{
+        public static float GeneralVolumen = 95.0f;
+		public static Manager_Input inputManager;
+		public static Manager_App appManager;
+		public static Manager_Scene sceneManager;
+        public static Manager_Audio audioManager;
+        public static Manager_GameState gameStateManager;
+        public static Manager_UI uiManager;
+	}
+}
---
+++ Assets/_game/MANGOS/Manager_UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mangos
+{
+	public class Manager_UI : MonoBehaviour {
+
+		void Awake()
+		{
+			Manager_Static.uiManager = this;
+		}
+	}
+}
