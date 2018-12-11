12dfe0942db53e2be69b69a32ae73f9071e176f5
Retus001 <retus001@gmail.com>
2018-12-11 17:04:02 -0600

    Scene & Movement Update
    Movement & Jump [ Alpha Done ]

--- Assets/_game/Arito/A_Scripts/CameraFollow.cs
+++ Assets/_game/Arito/A_Scripts/CameraFollow.cs
@@ -7,20 +7,29 @@ public class CameraFollow : MonoBehaviour {
     public Transform target;
     public float followSpeed;
 
-    [Header("Follow Constraits")]
+    [Header("Follow Axis")]
     public bool X;
     public bool Y;
     public bool Z;
 	
-	// Update is called once per frame
 	void Update () {
+        Follow(target);
+	}
+
+    public void SwitchTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void Follow(Transform _target)
+    {
         Vector3 finalPos = new Vector3(
-            X? target.position.x : transform.position.x,
-            Y? target.position.y : transform.position.y,
-            Z? target.position.z : transform.position.z
+            X ? _target.position.x : transform.position.x,
+            Y ? _target.position.y : transform.position.y,
+            Z ? _target.position.z : transform.position.z
             );
 
         Vector3 smoothedPos = Vector3.Lerp(transform.position, finalPos, followSpeed * Time.deltaTime);
         transform.position = smoothedPos;
-	}
+    }
 }
--- Assets/_game/Arito/A_Scripts/CharacterController.cs
+++ Assets/_game/Arito/A_Scripts/CharacterController.cs
@@ -8,20 +8,43 @@ namespace MANGOS
     {
         public float movementSpeed;
         public float runningMultiplier;
-        public float maxVelocity;
         public float jumpForce;
-        public LayerMask layerMask;
 
         public Rigidbody rig;
         private bool isAirborn = false;
         private float isRunning = 1;
         private float xDir, zDir;
+        private bool canMove = true;
 
-        private void OnTriggerEnter(Collider _col)
+        private void OnCollisionEnter(Collision _col)
         {
-            if (isAirborn)
-                if (_col.gameObject.CompareTag("Map"))
-                    isAirborn = false;
+            bool foundFloor = false;
+            for (int i = 0; i < _col.contacts.Length; i++)
+            {
+                if (!foundFloor)
+                {
+                    Vector3 normal = _col.contacts[i].normal.normalized;
+                    Vector3 up = Vector3.up;
+                    float comparison = Vector3.Dot(normal, up);
+                    if (comparison > 0.7f && _col.gameObject.CompareTag("Map"))
+                    {
+                        isAirborn = false;
+                        gameObject.GetComponent<CapsuleCollider>().material.staticFriction = 0.5f;
+                        foundFloor = true;
+                        canMove = true;
+                    }
+                    else if (comparison > 0.1f && comparison < 0.7f && _col.gameObject.CompareTag("Map"))
+                    {
+                        gameObject.GetComponent<CapsuleCollider>().material.staticFriction = 0.0f;
+                        canMove = false;
+                    }
+                    else
+                    {
+                        gameObject.GetComponent<CapsuleCollider>().material.staticFriction = 0.0f;
+                        canMove = true;
+                    }
+                }  
+            }
         }
 
         void Start()
@@ -37,22 +60,8 @@ namespace MANGOS
             else if (Input.GetKeyUp(KeyCode.LeftShift))
                 Run(false);
 
-            /*if (Input.GetKey(KeyCode.W))
-                zDir = 1;
-            else if (Input.GetKey(KeyCode.S))
-                zDir = -1;
-            else
-                zDir = 0;*/
-
             zDir = Input.GetAxis("Vertical");
             xDir = Input.GetAxis("Horizontal");
-
-            /*if (Input.GetKey(KeyCode.A))
-                xDir = -1;
-            else if (Input.GetKey(KeyCode.D))
-                xDir = 1;
-            else
-                xDir = 0;*/
 
             Move(xDir, zDir);
 
@@ -70,17 +79,15 @@ namespace MANGOS
 
         public void Move(float _xDir, float _zDir)
         {
-            Vector3 finalVel = rig.velocity;
+            if (canMove)
+            {
+                Vector3 finalVel = rig.velocity;
 
-            finalVel.x = _xDir * Time.deltaTime * movementSpeed * isRunning;
-            finalVel.z = _zDir * Time.deltaTime * movementSpeed * isRunning;
+                finalVel.x = _xDir * Time.deltaTime * movementSpeed * isRunning;
+                finalVel.z = _zDir * Time.deltaTime * movementSpeed * isRunning;
 
-            if (finalVel.x > maxVelocity)
-                finalVel.x = maxVelocity;
-            if (finalVel.z > maxVelocity)
-                finalVel.z = maxVelocity;
-
-            rig.velocity = finalVel;
+                rig.velocity = finalVel;
+            }
         }
 
         public void Jump()
@@ -90,6 +97,29 @@ namespace MANGOS
                 rig.AddForce(Vector3.up * jumpForce * 1000, ForceMode.Impulse);
                 isAirborn = true;
             }
+        }
+
+        /*public void Interact(GameObject _obj)
+        {
+            // ItemBehaviour
+            // type.HEAVY / type.LIGHT
+
+            _obj.GetComponent<ItemBehaviour>().type
+
+            switch(_obj)
+        }*/
+    }
+
+    public static class Extensions
+    {
+        public static float Remap(this float value, float inMin, float inMax, float outMin, float outMax)
+        {
+            return outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin);
+        }
+
+        public static Vector3 Remap(this Vector3 value, float inMin, float inMax, float outMin, float outMax)
+        {
+            return new Vector3(value.x.Remap(inMin, inMax, outMin, outMax), value.y.Remap(inMin, inMax, outMin, outMax), value.z.Remap(inMin, inMax, outMin, outMax));
         }
     }
 }
