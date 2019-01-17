34e5139a472789fb9c270dba9f980402e9fc0968
Retus001 <retus001@gmail.com>
2019-01-17 08:54:35 -0600

    Namespace Quickfix

--- Assets/_game/Arito/A_Scripts/CameraFollow.cs
+++ Assets/_game/Arito/A_Scripts/CameraFollow.cs
@@ -2,34 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
-
-    public Transform target;
-    public float followSpeed;
-
-    [Header("Follow Axis")]
-    public bool X;
-    public bool Y;
-    public bool Z;
-	
-	void Update () {
-        Follow(target);
-	}
-
-    public void SwitchTarget(Transform _target)
+namespace Mangos
+{
+    public class CameraFollow : MonoBehaviour
     {
-        target = _target;
-    }
 
-    private void Follow(Transform _target)
-    {
-        Vector3 finalPos = new Vector3(
-            X ? _target.position.x : transform.position.x,
-            Y ? _target.position.y : transform.position.y,
-            Z ? _target.position.z : transform.position.z
-            );
+        public Transform target;
+        public float followSpeed;
 
-        Vector3 smoothedPos = Vector3.Lerp(transform.position, finalPos, followSpeed * Time.deltaTime);
-        transform.position = smoothedPos;
+        [Header("Follow Axis")]
+        public bool X;
+        public bool Y;
+        public bool Z;
+
+        void Update()
+        {
+            Follow(target);
+        }
+
+        public void SwitchTarget(Transform _target)
+        {
+            target = _target;
+        }
+
+        private void Follow(Transform _target)
+        {
+            Vector3 finalPos = new Vector3(
+                X ? _target.position.x : transform.position.x,
+                Y ? _target.position.y : transform.position.y,
+                Z ? _target.position.z : transform.position.z
+                );
+
+            Vector3 smoothedPos = Vector3.Lerp(transform.position, finalPos, followSpeed * Time.deltaTime);
+            transform.position = smoothedPos;
+        }
     }
 }
--- Assets/_game/Arito/A_Scripts/CharacterController.cs
+++ Assets/_game/Arito/A_Scripts/CharacterController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MANGOS
+namespace Mangos
 {
     public class CharacterController : MonoBehaviour
     {
