21f0af0ede8d8b5f584e680da2165b696f5ab2f7
Galaxyben <galaxyben1@gmail.com>
2019-01-22 17:02:54 -0600

    Cambio de Ragdoll a Normal

--- Assets/_game/Items/Scripts/PlayerDataReceiver.cs
+++ Assets/_game/Items/Scripts/PlayerDataReceiver.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 namespace mangos
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerDataReceiver : MonoBehaviour
     {
         [Tooltip("El daño que le hará al PROP que le envió el golpe")]
@@ -12,25 +13,53 @@ namespace mangos
         [Tooltip("Porcentaje de daño que tiene actualmente")]
         public int percentage;
 
+        public bool Ragdoll;
+
 
         private Rigidbody m_rigi;
+        private Collider m_col;
+        private Rigidbody[] m_righijos;
+        private Collider[] m_colhijos;
+        
 
         void Start()    
         {
             m_rigi = GetComponent<Rigidbody>();
+            m_col = GetComponent<Collider>();
+            Ragdoll = false;
+            m_righijos = GetComponentsInChildren<Rigidbody>();
+            m_colhijos = GetComponentsInChildren<Collider>();
+            for(int i = 0; i < m_righijos.Length; i++)
+            {
+                m_righijos[i].isKinematic = true;
+            }
+            for(int i = 0; i < m_colhijos.Length; i++)
+            {
+                m_colhijos[i].enabled = false;
+            }
+            m_rigi.isKinematic = false;
+            m_col.enabled = true;
         }
 
 
         void Update()
         {
-
+            
         }
 
         void HitReceiver(ItemBehaviour data)
         {
             percentage = percentage + data.addingPercentage;
-            
+            if(percentage >= 120)
+            {
+                ActivateRagdoll();
+            }
             data.GetHit(dañoDevuelta);
+        }
+
+        public void ActivateRagdoll()
+        {
+
         }
     }
 }
