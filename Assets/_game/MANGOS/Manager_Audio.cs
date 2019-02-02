efc984c37d2a8500d681800dd88ac01a117268c1
Galaxyben <galaxyben1@gmail.com>
2019-02-02 00:30:10 -0600

    Fetcheee de 2 pendejadas de Lalo en la cual hacía un sólo debug

--- Assets/_game/MANGOS/Manager_Audio.cs
+++ Assets/_game/MANGOS/Manager_Audio.cs
@@ -2,115 +2,172 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Mangos 
+
+namespace Mangos
 {
-    public enum sounds : int
+    public enum Sounds : int
     {
-        
+        TESLA_SHOCK,
+        STANDARD_BULLET,
+        PLAYER_SPECIAL,
+        ENEMY_DIE,
+        TURRET_LEVELUP,
+        ICE_BALL,
+        PLAYER_BULLET,
+        PLAYER_DAMAGED,
+        TURRET_SET,
+        MENU_BUTTON,
+        MENU_BACK,
+        MENU_MUSIC,
+        GAME_MUSIC,
+        WIN,
+        LOSE
     }
 
-	public class Manager_Audio : MonoBehaviour {
-
-		[Tooltip("Es un emptyObject con un audio Soruce")]
-		public GameObject audioDad;
-		public Camera cam;
-		public AudioClip mainMenu;
+    public class Manager_Audio : MonoBehaviour
+    {
+        public float masterVolume = 1;
+        [Tooltip("Es un emptyObject con un audio Soruce")]
+        public GameObject audioDad;
+        public Camera cam;
+        public AudioClip mainMenu;
 
         public AudioClip[] clips;
+        public int[] groupSizes;
+        private int[] clipIndex;
 
-		void SetVolumenGeneral(float value)
-		{
-			Manager_Static.GeneralVolumen = value;
-		}
-
-		void Awake()
-		{
-			Manager_Static.audioManager = this;
-		}
-
-		void Start()
-		{
-			if (Manager_Static.gameStateManager.gameState == GameState.MAIN_MENU)
-				PlayMusic (mainMenu);
-		}
-
-		public void PlaySoundAt (Vector3 pos, AudioClip clip)
-		{
-			GameObject sound = Instantiate (audioDad, pos, Quaternion.identity);
-			sound.GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-			sound.GetComponent<AudioSource> ().PlayOneShot (clip);
-			Destroy (sound, clip.length + 0.1f);
-		}
-
-        public void PlaySoundAt(Vector3 pos, sounds clip)
+        void SetVolumenGeneral(float value)
         {
-            GameObject sound = Instantiate(audioDad, pos, Quaternion.identity);
-			sound.GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-            sound.GetComponent<AudioSource>().PlayOneShot(clips[(int)clip]);
-            Destroy(sound, clips[(int)clip].length + 0.1f);
+            Manager_Static.GeneralVolumen = value;
+        }
+
+        void Awake()
+        {
+            Manager_Static.audioManager = this;
+        }
+
+        void Start()
+        {
+            SetVolumenGeneral(1);
+
+            //Getting clip group index for easier use
+            clipIndex = new int[groupSizes.Length];
+            int counter = 0;
+            for (int i = 0; i < groupSizes.Length; i++)
+            {
+                clipIndex[i] = counter;
+                counter += groupSizes[i];
+            }
+            //Setting cam if not publicly set
+            if (!cam)
+                cam = Camera.main;
+            //Making sure audio dad is set up correctly
+            if (!audioDad.GetComponent<AudioDad>())
+            {
+                audioDad.AddComponent<AudioDad>();
+            }
+            //Making pools and prespawning
+            PoolManager.MakePool(audioDad);
+            PoolManager.SetPoolLimit(audioDad, clips.Length * 50);
+            PoolManager.PreSpawn(audioDad, clips.Length * 6, false);
+        }
+
+        public void PlaySoundAt(Vector3 pos, AudioClip clip)
+        {
+            Transform sound = PoolManager.Spawn(audioDad, pos, Quaternion.identity);
+            AudioSource temp = sound.GetComponent<AudioSource>();
+            temp.volume = Manager_Static.GeneralVolumen / 100;
+            temp.clip = clip;
+            temp.PlayOneShot(clip);
+            sound.GetComponent<AudioDad>().Despawn(clip.length);
+        }
+
+        public void PlaySoundAt(Vector3 pos, Sounds clip)
+        {
+            int index = Random.Range(clipIndex[(int)clip], clipIndex[(int)clip] + groupSizes[(int)clip]);
+
+            Transform sound = PoolManager.Spawn(audioDad, pos, Quaternion.identity);
+            AudioSource temp = sound.GetComponent<AudioSource>();
+            temp.volume = Manager_Static.GeneralVolumen / 100;
+            temp.PlayOneShot(clips[index]);
+            sound.GetComponent<AudioDad>().Despawn(clips[index].length);
         }
 
         public void PlaySoundGlobal(AudioClip clip)
-		{
-			GameObject sound = Instantiate (audioDad, cam.transform.position, Quaternion.identity, cam.transform);
-			sound.GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-			sound.GetComponent<AudioSource> ().PlayOneShot (clip);
-			Destroy (sound, clip.length + 0.1f);
-		}
-
-        public void PlaySoundGlobal(sounds clip)
         {
-            GameObject sound = Instantiate(audioDad, cam.transform.position, Quaternion.identity, cam.transform);
-			sound.GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-            sound.GetComponent<AudioSource>().PlayOneShot(clips[(int)clip]);
-            Destroy(sound, clips[(int)clip].length + 0.1f);
+            Transform sound = PoolManager.Spawn(audioDad, cam.transform.position, Quaternion.identity);
+            sound.parent = cam.transform;
+            AudioSource temp = sound.GetComponent<AudioSource>();
+            temp.volume = Manager_Static.GeneralVolumen / 100;
+            temp.PlayOneShot(clip);
+            sound.GetComponent<AudioDad>().Despawn(clip.length);
+        }
+
+        public void PlaySoundGlobal(Sounds clip)
+        {
+            int index = Random.Range(clipIndex[(int)clip], clipIndex[(int)clip] + groupSizes[(int)clip]);
+
+            Transform sound = PoolManager.Spawn(audioDad, cam.transform.position, Quaternion.identity);
+            sound.parent = cam.transform;
+            AudioSource temp = sound.GetComponent<AudioSource>();
+            temp.volume = Manager_Static.GeneralVolumen / 100;
+            temp.PlayOneShot(clips[index]);
+            sound.GetComponent<AudioDad>().Despawn(clips[index].length);
         }
 
         public void PlayMusic(AudioClip clip)
-		{
-			if (!GameObject.Find("jukebox"))
-			{
-				GameObject jukebox = Instantiate (audioDad, cam.transform.position, Quaternion.identity, cam.transform);
-				jukebox.gameObject.name = "jukebox";
-				jukebox.GetComponent<AudioSource> ().clip = clip;
-				jukebox.GetComponent<AudioSource> ().loop = true;
-				jukebox.GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-				jukebox.GetComponent<AudioSource> ().Play ();
-			}
-			else
-			{
-				GameObject.Find ("jukebox").GetComponent<AudioSource> ().clip = clip;
-				GameObject.Find("jukebox").GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-				GameObject.Find ("jukebox").GetComponent<AudioSource> ().Play();
-			}
-		}
-
-        public void PlayMusic(sounds clip)
         {
             if (!GameObject.Find("jukebox"))
             {
-                GameObject jukebox = Instantiate(audioDad, cam.transform.position, Quaternion.identity, cam.transform);
+                Transform jukebox = PoolManager.Spawn(audioDad, cam.transform.position, Quaternion.identity);
+                jukebox.parent = cam.transform;
                 jukebox.gameObject.name = "jukebox";
-                jukebox.GetComponent<AudioSource>().clip = clips[(int)clip];
-                jukebox.GetComponent<AudioSource>().loop = true;
-				jukebox.GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-                jukebox.GetComponent<AudioSource>().Play();
+                AudioSource temp = jukebox.GetComponent<AudioSource>();
+                temp.clip = clip;
+                temp.loop = true;
+                temp.volume = Manager_Static.GeneralVolumen / 100;
+                temp.Play();
             }
             else
             {
-                GameObject.Find("jukebox").GetComponent<AudioSource>().clip = clips[(int)clip];
-				GameObject.Find("jukebox").GetComponent<AudioSource> ().volume = Manager_Static.GeneralVolumen / 100;
-                GameObject.Find("jukebox").GetComponent<AudioSource>().Play();
+                AudioSource temp = GameObject.Find("jukebox").GetComponent<AudioSource>();
+                temp.clip = clip;
+                temp.volume = Manager_Static.GeneralVolumen / 100;
+                temp.Play();
+            }
+        }
+
+        public void PlayMusic(Sounds clip)
+        {
+            int index = Random.Range(clipIndex[(int)clip], clipIndex[(int)clip] + groupSizes[(int)clip]);
+
+            if (!GameObject.Find("jukebox"))
+            {
+                Transform jukebox = PoolManager.Spawn(audioDad, cam.transform.position, Quaternion.identity);
+                jukebox.parent = cam.transform;
+                jukebox.gameObject.name = "jukebox";
+                AudioSource temp = jukebox.GetComponent<AudioSource>();
+                temp.clip = clips[(int)clip];
+                temp.loop = true;
+                temp.volume = Manager_Static.GeneralVolumen / 100;
+                temp.Play();
+            }
+            else
+            {
+                AudioSource temp = GameObject.Find("jukebox").GetComponent<AudioSource>();
+                temp.clip = clips[(int)clip];
+                temp.volume = Manager_Static.GeneralVolumen / 100;
+                temp.Play();
             }
         }
 
         public void StopMusic()
-		{
-			if (GameObject.Find("jukebox"))
-			{
-				GameObject.Find ("jukebox").GetComponent<AudioSource> ().Stop ();
-				Destroy (GameObject.Find("jukebox"));
-			}
-		}
-	}
+        {
+            GameObject jukebox = GameObject.Find("jukebox");
+            if (jukebox)
+            {
+                jukebox.GetComponent<AudioDad>().Despawn();
+            }
+        }
+    }
 }
