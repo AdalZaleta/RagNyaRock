5a06780225eb621a5e717e184c660840bd362394
Galaxyben <galaxyben1@gmail.com>
2019-02-05 13:29:27 -0600

    Audio Espacio

---
+++ Assets/_game/Items/Scripts/EspacioSonidos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+    public class EspacioSonidos : MonoBehaviour
+    {
+        void Start()
+        {
+            Manager_Static.audioManager.PlaySoundGlobal(Sounds.ESPACIO_SONIDOS_Y_MUSICA);
+        }
+    }
+}
